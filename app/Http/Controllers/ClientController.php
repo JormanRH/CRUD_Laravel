a8cfb20511c9b04f3d5d6099000edb5228f7afc6
Jorman <jormanrh1993@gmail.com>
2023-12-20 11:50:24 -0600

    Prueba de carga por VSC

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $client = Client::paginate(5);//all
+        return view('Client.index')->with('clients', $client);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('Client.form');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:15',
+            'due' => 'required|gte:1'
+        ]);
+        $Client = Client::create($request->only('name', 'due', 'comments'));
+        //return 'Vamos a Guardar';
+        Session::flash('Mensaje', 'Registro creado con éxito!');
+        
+        return redirect()->route('client.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Client $client)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Client $client)
+    {
+        return view('Client.form')->with('client', $client);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Client $client)
+    {
+        $request->validate([
+            'name' => 'required|max:15',
+            'due' => 'required|gte:1'
+        ]);
+        $client->name = $request['name'];
+        $client->due = $request['due'];
+        $client->comments = $request['comments'];
+        $client->save();
+        //return 'Vamos a Guardar';
+        Session::flash('Mensaje', 'Registro editado con éxito!');
+        
+        return redirect()->route('client.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Client $client)
+    {
+        $client->delete();
+        Session::flash('Mensaje', 'Registro eliminado con éxito!');
+        
+        return redirect()->route('client.index');
+    }
+}
